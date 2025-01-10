adcaeee08a3bd4345162cbbb32d6681c04df0460
david2am <36830835+david2am@users.noreply.github.com>
2025-01-10 08:47:11 -0500

    feat/Navbar - add login and logout functionality

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,38 @@
-export default function  () {
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function  () {
+    const session = await auth()
     return (
-        <section>Navbar</section>
+        <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+            <nav className="flex justify-between items-center">
+                <Link href="/">
+                    <Image src="/logo.png" alt="logo" width={144} height={30} />
+                </Link>
+
+                <div className="flex items-center gap-5 text-black">
+                    {session && session?.user ? (
+                        <>
+                            <Link href="/startup/create">Create</Link>
+                            <form action={async () => {
+                                'use server'
+                                await signOut({ redirectTo: '/' })
+                            }}>
+                                <button type="submit">Logout</button>
+                            </form>
+                            <Link href={`/user/${session?.id}`}>{session?.user?.name}</Link>
+                        </>
+                    ) : (
+                        <form action={async () => {
+                            'use server'
+                            await signIn('github')
+                        }}>
+                            <button type="submit">Login</button>
+                        </form>
+                    )}
+                </div>
+            </nav>
+        </header>
     )
 }
