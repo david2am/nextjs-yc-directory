af0e12de4251518cb612bc78bda2d7e277588e4b
david2am <36830835+david2am@users.noreply.github.com>
2025-01-13 17:36:18 -0500

    feat app/root - build hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import SearchForm from "@/app/components/SearchForm";
 
 export default function Home() {
   return (
     <>
-      <h1 className="heading">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit ideas, Vote on Pitches, and Get Noticed in Virtual Competitiones.
+        </p>
+        <SearchForm />
+      </section>
     </>
   );
 }
